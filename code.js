2bf1e15ab4915f01610a23f8982578c05f8aacbe
Nicolas-Madronero <139486162+Nicolas-Madronero@users.noreply.github.com>
2025-04-27 23:28:51 -0400

    Update code.js

---
+++ code.js
@@ -0,0 +1,45 @@
+
+//TASK 2 code
+function cca(){
+	document.getElementById("optA").style.fontFamily = "arial";
+	document.getElementById("optA").style.color = "black";
+  
+}
+
+function rca(){
+	document.getElementById("optA").style.color = "";
+  document.getElementById("optA").style.fontFamily = "times new roman";
+}
+
+function ccb(){
+	document.getElementById("optB").style.fontFamily = "arial";
+	document.getElementById("optB").style.color = "black";
+  
+}
+
+function rcb(){
+	document.getElementById("optB").style.color = "";
+  document.getElementById("optB").style.fontFamily = "times new roman";
+}
+
+function ccc(){
+	document.getElementById("optC").style.fontFamily = "arial";
+	document.getElementById("optC").style.color = "black";
+  
+}
+
+function rcc(){
+	document.getElementById("optC").style.color = "";
+  document.getElementById("optC").style.fontFamily = "times new roman";
+}
+
+function ccd(){
+	document.getElementById("optD").style.fontFamily = "arial";
+	document.getElementById("optD").style.color = "black";
+  
+}
+
+function rcd(){
+	document.getElementById("optD").style.color = "";
+  document.getElementById("optD").style.fontFamily = "times new roman";
+}
